e64fcbc29d95e20ba75faff63a5cae8f7a4e5b01
OmarZaky <omarzaky078@gmail.com>
2025-08-31 14:22:47 +0300

    Start Angular Structrue At newbee.cs

--- Newbee.UI/projects/newbee.cs/src/app/app-routing-module.ts
+++ Newbee.UI/projects/newbee.cs/src/app/app-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//LazyLoading The Cart Module
+const routes: Routes = [
+  {path:'cart',loadChildren:()=>import('./features/cart/cart-module').then(m=>m.CartModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
