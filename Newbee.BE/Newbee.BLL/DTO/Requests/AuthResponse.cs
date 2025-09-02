c03b70bad080f05a43a8e2953946d5d18af3c0fb
Muhammed Khaled <mohammedk7alid2004@gmail.com>
2025-09-02 20:46:00 +0300

    add refresh token services

--- Newbee.BE/Newbee.BLL/DTO/Requests/AuthResponse.cs
+++ Newbee.BE/Newbee.BLL/DTO/Requests/AuthResponse.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Newbee.BLL.DTO.Authentication;
+public record AuthResponse
+(
+    int  UserId,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Token,
+    int ExpiresIn,
+    string RefreshToken ,
+    DateTime RefreshTokenExpiration
 
-namespace Newbee.BLL.DTO.Authentication
-{
-    public record AuthResponse
-    (
-        int  UserId,
-        string Email,
-        string FirstName,
-        string LastName,
-        string Token,
-        int ExpiresIn
-        );
-    
-}
+);
+
