61e54de6f65585d7f88b9e7d1044d1c6e3f5666d
Muhammed Khaled <mohammedk7alid2004@gmail.com>
2025-09-03 11:33:31 +0300

    update auth services and update auth controller

---
+++ Newbee.BE/Newbee.BLL/Interfaces/ICustomerAuthServices.cs
@@ -0,0 +1,16 @@
+﻿using Newbee.BLL.DTO.Auth.Responses;
+using Newbee.BLL.DTO.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newbee.BLL.Interfaces
+{
+    public interface ICustomerAuthServices
+    {
+        Task<AuthResponse?> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
+        Task<Result> ResendConfirmationEmailAsync(MailRequest request, CancellationToken cancellationToken = default);
+    }
+}
