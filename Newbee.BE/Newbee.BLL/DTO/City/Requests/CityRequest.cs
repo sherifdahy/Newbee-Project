92ca621e84b0e5800f698f79d5cad372bf3f3717
sherif <sherifelgendy2004@gmail.com>
2025-09-04 21:57:57 +0300

    done_three_fromAddress

---
+++ Newbee.BE/Newbee.BLL/DTO/City/Requests/CityRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newbee.BLL.DTO.City.Requests
+{
+    public class CityRequest
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int CountryId { get; set; }
+    }
+}
---
+++ Newbee.BE/Newbee.BLL/Errors/ZoneErrors.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Newbee.BLL.Errors
+{
+    public static class ZoneErrors
+    {
+        public static readonly Error NotFound =
+            new Error("Zone.NotFound", "Zone does not exist.", StatusCodes.Status404NotFound);
+
+        public static readonly Error InvalidId =
+            new Error("Zone.InvalidId", "Invalid Zone ID.", StatusCodes.Status400BadRequest);
+
+        public static readonly Error DuplicatedZone =
+            new Error("Zone.DuplicatedZone", "Zone already exists.", StatusCodes.Status409Conflict);
+
+        public static readonly Error CityNotFound =
+            new Error("Zone.CityNotFound", "Associated city does not exist.", StatusCodes.Status404NotFound);
+    }
+}
