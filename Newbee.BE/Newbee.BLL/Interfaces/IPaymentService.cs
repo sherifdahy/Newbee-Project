958613d117b2140ad639c423dda0b48130588cb7
Muhammed Khaled <mohammedk7alid2004@gmail.com>
2025-09-06 16:02:33 +0300

    Add files via upload

---
+++ Newbee.BE/Newbee.BLL/Interfaces/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newbee.BLL.Interfaces;
+
+public interface IPaymobService
+{
+    Task<(Order Order, string RedirectUrl)> ProcessPaymentAsync(int orderId, string paymentMethod, decimal amount);
+    Task<Order> UpdateOrderSuccess(string specialReference);
+    Task<Order> UpdateOrderFailed(string specialReference);
+    string ComputeHmacSHA512(string data, string secret);
+}
